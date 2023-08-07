441e735716ca559a3f4a9238bbd5776c0e515d10
KDA <kuda74ru@gmail.ru>
2023-08-07 21:53:29 +0500

    add Task3

---
+++ Tasks/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая будет
+// преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string result = "";
+
+while (num > 0)
+{
+    result = num % 2 + result;
+    num /= 2;
+}
+
+Console.WriteLine(result);
