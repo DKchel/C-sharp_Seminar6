e57e89557fca24831d4b997452474c53bace8423
KDA <kuda74ru@gmail.ru>
2023-08-07 21:01:57 +0500

    add Task1

---
+++ Tasks/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+// int[] array = new int [] { 1, 2, 3, 4, 5};
+
+int[] array = { 1, 3, 5, 8, 9, 7, 4, 6, 2 };
+
+void ReversArray()
+{
+    for (int i = 0; i < array.Length / 2; i++)   // потому, что меняем половину массива, иначе вернёт всё на место.
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = temp;
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+
+}
+
+PrintArray();
+Console.WriteLine();
+ReversArray();
+PrintArray();
