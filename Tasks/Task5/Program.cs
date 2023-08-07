6a5ecdd222fbb8af0ce00037964a081b00f4462b
KDA <kuda74ru@gmail.ru>
2023-08-07 22:19:22 +0500

    add Task5

---
+++ Tasks/Task5/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая будет создавать
+// копию заданного массива с помощью поэлементного копирования.
+
+
+
+int[] array =  { 51, 42, 13, 9 };
+int[] array2 = new int [array.Length];
+
+void CopyArray()
+{
+    for (int i = 0; i < array2.Length; i++)
+    {
+        array2[i] = array[i];
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array2.Length; i++)
+    {
+        Console.Write(array2[i] + " ");
+    }
+}
+
+CopyArray();
+PrintArray();
